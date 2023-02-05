0b7e8d14603191564656d4d63406c1130d44ec49
Ludot13 <gammajdo16@mail.ru>
2023-02-05 18:44:49 +0300

    main

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-print("Hello world!")
+# print("Hello world!")
 
+# Задача №1
+# a = input()
+#
+# print( a[:a.find('h')] + a[a.rfind('h') +1:])
 
+# Задача №2
+
+s = input()
+a = s[:s.find('h')]
+b = s[s.find('h'):s.rfind('h') + 1]
+c = s[s.rfind('h') + 1:]
+s = a + b[::-1] + c
+print(s)
+
+# Задача №3
+
+# s = "11 23 44 55 23 22"
+# str1 = input("Введите заменяемую подстроку: ")
+# str2 = input("Новая подстрока: ")
+# print(s.replace(str1, str2))
+
+# Задача №4
+
+# s = """
+# Ежевику для ежат
+# Принесли два ежа.
+# Ежевику еле-еле
+# Ежата возле ели съели
+# """
+# col = 0
+# for i in s.lower().split():
+#     if i.startswith("е"):
+#         col += 1
+# print(f"Количество слов: {col}")
